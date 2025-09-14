a8fd215d3f9efd8a887397760f2a193a3c201f65
lukada-taiya <lukmanadam43@gmail.com>
2025-09-14 22:34:11 +0000

    Feat: Refactored repository abstraction into the application layer

--- InventoryX.Application/Repository/IBaseRepository.cs
+++ InventoryX.Application/Repository/IBaseRepository.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
-namespace InventoryX.Infrastructure.Persistence
+namespace InventoryX.Application.Repository
 {
     public interface IBaseRepository<TEntity>
     {
--- InventoryX.Application/Repository/ISalePurchaseRepository.cs
+++ InventoryX.Application/Repository/ISalePurchaseRepository.cs
@@ -1,12 +1,6 @@
-﻿using InventoryX.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
-namespace InventoryX.Infrastructure.Persistence
+namespace InventoryX.Application.Repository
 {
     public interface ISalePurchaseRepository<T> where T : class
     {
