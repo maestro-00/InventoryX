225a44b3b38ccae681e4ba4eb250051ccba94ec4
lukada-taiya <lukmanadam43@gmail.com>
2025-09-21 17:21:49 +0000

    Enhancement: Improved test coverage for CreateInventoryItemCommandHandler.cs

--- tests/InventoryX.Application.Tests/Commands/RequestHandlers/InventoryItems/CreateInventoryItemCommandHandlerTests.cs
+++ tests/InventoryX.Application.Tests/Commands/RequestHandlers/InventoryItems/CreateInventoryItemCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 using InventoryX.Application.Commands.RequestHandlers.InventoryItems;
 using InventoryX.Application.Commands.Requests.InventoryItems;
+using Microsoft.AspNetCore.Http;
 
 namespace InventoryX.Application.Tests.Commands.RequestHandlers.InventoryItems;
 
@@ -10,6 +11,10 @@ public class CreateInventoryItemCommandHandlerTests
     private readonly Mock<IRetailStockService> _retailStockMock;
     private readonly CreateInventoryItemCommandHandler _sut;
     private readonly Mock<IMapper>? _mapperMock;
+    private readonly int _newlyCreatedIdResponse;
+    private readonly int _failedResponse;
+    private readonly CreateInventoryItemCommand? _createCommand;
+    private readonly CancellationToken _cancellationToken;
 
     public CreateInventoryItemCommandHandlerTests()
     {
@@ -21,184 +26,141 @@ public class CreateInventoryItemCommandHandlerTests
         var inventoryItem = _fixture.Create<InventoryItem>();
         _mapperMock.Setup(m => m.Map<InventoryItem>(It.IsAny<InventoryItemCommandDto>()))
             .Returns(inventoryItem);
+        _newlyCreatedIdResponse = 1;
+        _failedResponse = -1;
+        _createCommand = _fixture.Create<CreateInventoryItemCommand>();
+        _cancellationToken = _fixture.Create<CancellationToken>();
     }
 
     [Fact]
     public async Task Handle_WhenCalled_ShouldCallAddInventoryItemMethod()
     {
-        var genericResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericResponse);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericResponse);
+            .ReturnsAsync(_newlyCreatedIdResponse);
+        // _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
+        //     .ReturnsAsync(_successResponse);
         
-        await _sut.Handle(command, cancellationToken);
+        await _sut.Handle(_createCommand, _cancellationToken);
         
-        _serviceMock.Verify(it => it.AddInventoryItem(It.IsAny<InventoryItem>()), Times.Once);
-    }
-    
-    [Fact]
-    public async Task Handle_WhenCalled_ShouldUpdateCreatedAtOfBothInventoryItemAndRetailStock()
-    {
-        var genericResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
-        _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericResponse);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericResponse);
-        
-        await _sut.Handle(command, cancellationToken);
-        
-        _serviceMock.Verify(it => it.AddInventoryItem(It.Is<InventoryItem>( r => r.Created_At != null)), Times.Once);
-        _retailStockMock.Verify(it => it.AddRetailStock(It.Is<RetailStock>( r => r.Created_At != null)), Times.Once);
+        _serviceMock.Verify(it => it.AddInventoryItem(It.Is<InventoryItem>(i => 
+            i.Created_At != null &&
+            i.Created_At.Value.Date == DateTime.UtcNow.Date
+            )), Times.Once);
     }
     
     [Fact]
     public async Task Handle_WhenAddInventoryItemServiceSuccessful_ShouldCallAddRetailStockMethod()
     {
-        var successfulInventoryItemResponse = 1;
-        var genericResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>(); 
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(successfulInventoryItemResponse);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericResponse); 
+            .ReturnsAsync(_newlyCreatedIdResponse);
         
+        await _sut.Handle(_createCommand, _cancellationToken);
         
-        await _sut.Handle(command, cancellationToken);
-        
-        _retailStockMock.Verify(it => it.AddRetailStock(It.IsAny<RetailStock>()), Times.Once);
+        _retailStockMock.Verify(it => it.AddRetailStock(It.Is<RetailStock>(r =>
+            r.InventoryItemId == _newlyCreatedIdResponse &&
+            r.Quantity == _createCommand.RetailQuantity &&
+            r.Created_At != null &&
+            r.Created_At.Value.Date == DateTime.UtcNow.Date
+            )), Times.Once);
     }
     
     [Fact]
     public async Task Handle_WhenAddInventoryItemServiceFailed_ShouldReturnFailedApiResponse()
     {
-        var failedInventoryItemResponse = -1;
-        var genericResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(failedInventoryItemResponse);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericResponse); 
+            .ReturnsAsync(_failedResponse);
+        // _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
+        //     .ReturnsAsync(genericResponse); 
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+        var result = await _sut.Handle(_createCommand, _cancellationToken);
 
-        result.Success.Should().Be(false);
-        result.StatusCode.Should().Be(500); 
+        result.Should().NotBeNull();
+        result.Success.Should().BeFalse();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError); 
     }
     
     [Fact]
     public async Task Handle_WhenAddRetailStockServiceFailed_ShouldReturnFailedApiResponse()
     {
-        var failedRetailStockResponse = -1;
-        var genericResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericResponse);
+            .ReturnsAsync(_newlyCreatedIdResponse);
         _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(failedRetailStockResponse); 
+            .ReturnsAsync(_failedResponse); 
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+        var result = await _sut.Handle(_createCommand, _cancellationToken);
 
-        result.Success.Should().Be(false);
-        result.StatusCode.Should().Be(500); 
+        result.Should().NotBeNull();
+        result.Success.Should().BeFalse();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError); 
     }
     
     [Fact]
-    public async Task Handle_WhenBothAddInventoryItemServiceAndAddRetailStockServiceSuccessful_ShouldReturnSuccessApiResponse()
+    public async Task Handle_WhenBothInventoryItemAndRetailStockAddedSuccessful_ShouldReturnSuccessApiResponse()
     { 
-        var genericSuccessResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
+        var inventoryItem = _fixture.Create<InventoryItem>();
+        _mapperMock.Setup(m => m.Map<InventoryItem>(It.IsAny<InventoryItemCommandDto>()))
+            .Returns(inventoryItem);
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericSuccessResponse);
+            .ReturnsAsync(_newlyCreatedIdResponse);
         _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericSuccessResponse); 
+            .ReturnsAsync(_newlyCreatedIdResponse); 
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+        var result = await _sut.Handle(_createCommand, _cancellationToken);
 
-        result.Success.Should().Be(true); result.StatusCode.Should().Be(201); 
+        result.Should().NotBeNull();
+        result.Id.Should().Be(_newlyCreatedIdResponse);
+        result.Success.Should().BeTrue(); 
+        result.StatusCode.Should().Be(StatusCodes.Status201Created); 
     }
     
     [Fact]
     public async Task Handle_WhenCalled_ShouldMapCorrectDto()
     { 
-        var genericSuccessResponse = 1;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
-        _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericSuccessResponse);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericSuccessResponse); 
+        // _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
+        //     .ReturnsAsync(_successResponse);
+        // _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
+        //     .ReturnsAsync(_failedResponse); 
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+         await _sut.Handle(_createCommand, _cancellationToken);
 
-        _mapperMock.Verify(x => x.Map<InventoryItem>(command.NewInventoryItemDto), Times.Once);
-    }
-    
-    [Fact]
-    public async Task Handle_WhenCalled_ShouldReturnNewInventoryItemIdInApiResponse()
-    { 
-        var genericSuccessResponse = 1;
-        var expectedNewId = 2;
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
-        _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(expectedNewId);
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericSuccessResponse); 
-        
-        
-        var result = await _sut.Handle(command, cancellationToken);
-
-        result.Id.Should().Be(expectedNewId);
+        _mapperMock.Verify(x => x.Map<InventoryItem>(_createCommand.NewInventoryItemDto), Times.AtLeastOnce);
     }
     
     [Fact]
     public async Task Handle_WhenAddInventoryItemThrowsException_ShouldReturnFailedApiResponse()
     { 
-        var genericSuccessResponse = 1; 
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
             .ThrowsAsync(new Exception("Exception thrown"));
-        _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
-            .ReturnsAsync(genericSuccessResponse); 
+        // _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
+        //     .ReturnsAsync(genericSuccessResponse); 
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+        var result = await _sut.Handle(_createCommand, _cancellationToken);
 
-        result.Success.Should().Be(false);
-        result.StatusCode.Should().Be(500);
+        result.Should().NotBeNull();
+        result.Success.Should().BeFalse();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
         result.Message.Should().Contain("Exception");
     }
     
     [Fact]
     public async Task Handle_WhenAddRetailStockThrowsException_ShouldReturnFailedApiResponse()
     { 
-        var genericSuccessResponse = 1; 
-        var command = _fixture.Create<CreateInventoryItemCommand>();
-        var cancellationToken = _fixture.Create<CancellationToken>();
         _serviceMock.Setup(m => m.AddInventoryItem(It.IsAny<InventoryItem>()))
-            .ReturnsAsync(genericSuccessResponse); 
+            .ReturnsAsync(_newlyCreatedIdResponse); 
         _retailStockMock.Setup(m => m.AddRetailStock(It.IsAny<RetailStock>()))
             .ThrowsAsync(new Exception("Exception thrown"));
         
         
-        var result = await _sut.Handle(command, cancellationToken);
+        var result = await _sut.Handle(_createCommand, _cancellationToken);
 
-        result.Success.Should().Be(false);
-        result.StatusCode.Should().Be(500);
+        result.Should().NotBeNull();
+        result.Success.Should().BeFalse();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
         result.Message.Should().Contain("Exception");
     }
     
