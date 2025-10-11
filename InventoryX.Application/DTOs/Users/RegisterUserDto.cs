5f5e144a70065cbd1e3e07b03fe7a20eb9abe18f
lukada-taiya <lukmanadam43@gmail.com>
2025-10-11 08:30:21 +0000

    Override register endpoint to include User's name
    # Conflicts:
    #	InventoryX.Presentation/Program.cs
    
    # Conflicts:
    #	InventoryX.Application/DTOs/Users/RegisterUserDto.cs

--- InventoryX.Application/DTOs/Users/RegisterUserDto.cs
+++ InventoryX.Application/DTOs/Users/RegisterUserDto.cs
@@ -1,16 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InventoryX.Domain.Models;
 
 namespace InventoryX.Application.DTOs.Users
 {
-    class RegisterUserDto
+    public class RegisterUserDto
     {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public required string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
     }
 }
