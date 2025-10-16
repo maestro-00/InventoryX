30069b2f308e4b2100d93cd6c95483acdf06ac3a
lukada-taiya <lukmanadam43@gmail.com>
2025-10-16 11:25:22 +0000

    enhancement: add reset and email verification url to appsettings

--- InventoryX.Application/Options/AuthOptions.cs
+++ InventoryX.Application/Options/AuthOptions.cs
@@ -4,4 +4,6 @@ public class AuthOptions
 {
     //Url to append code and userId to when verifying user email
     public string? EmailVerificationUrl { get; set; }
+    //Url to append code and email when resetting user password
+    public string? ResetPasswordUrl { get; set; }
 }
