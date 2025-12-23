afa8f7aa7bd35c9281bdc9ee006f48b3546e3e2a
lukada-taiya <lukmanadam43@gmail.com>
2025-12-23 12:22:15 +0000

    enhancement: add SubTotal property to sale dtos

---
+++ InventoryX.Infrastructure/Migrations/20251113164057_SubTotalToSale.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryX.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubTotalToSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "SubTotal",
+                table: "Sales",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubTotal",
+                table: "Sales");
+        }
+    }
+}
---
+++ InventoryX.Infrastructure/Migrations/20251128005256_AddSaleGroupEntity.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryX.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSaleGroupEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_SaleGroup_SaleGroupId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SaleGroup",
+                table: "SaleGroup");
+
+            migrationBuilder.RenameTable(
+                name: "SaleGroup",
+                newName: "SaleGroups");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SaleGroups",
+                table: "SaleGroups",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_SaleGroups_SaleGroupId",
+                table: "Sales",
+                column: "SaleGroupId",
+                principalTable: "SaleGroups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_SaleGroups_SaleGroupId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SaleGroups",
+                table: "SaleGroups");
+
+            migrationBuilder.RenameTable(
+                name: "SaleGroups",
+                newName: "SaleGroup");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SaleGroup",
+                table: "SaleGroup",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_SaleGroup_SaleGroupId",
+                table: "Sales",
+                column: "SaleGroupId",
+                principalTable: "SaleGroup",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
