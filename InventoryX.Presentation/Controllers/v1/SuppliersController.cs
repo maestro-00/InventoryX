300e5f4bff3003bf24edc405c39fb65e9089e6fe
lukada-taiya <lukmanadam43@gmail.com>
2026-07-30 21:04:20 +0000

    feat: expose supplier management (T094)
    Closes #205

---
+++ InventoryX.Presentation/Controllers/v1/SuppliersController.cs
@@ -0,0 +1,7 @@
+using InventoryX.Application.Commands.Requests.Purchasing; using InventoryX.Application.Repository; using MediatR; using Microsoft.AspNetCore.Authorization; using Microsoft.AspNetCore.Mvc; using Microsoft.EntityFrameworkCore;
+namespace InventoryX.Presentation.Controllers.v1;
+[Route("api/v1/suppliers")][Authorize]
+public sealed class SuppliersController(ISender sender,IAppDbContext context):ApiControllerBase{
+ [HttpGet] public async Task<ActionResult> List(CancellationToken ct)=>Ok(await context.Suppliers.AsNoTracking().Select(x=>new SupplierDto(x.Id,x.Name,x.Email,x.Phone)).ToListAsync(ct));
+ [HttpPost] public async Task<ActionResult<SupplierDto>> Create(CreateSupplierCommand command,CancellationToken ct)=>Ok(await sender.Send(command,ct));
+}
